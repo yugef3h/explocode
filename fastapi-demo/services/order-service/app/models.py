c7eb495180661f12a46d43d5a55d1d869cedd49d
yugef3h <ql1993xl@gmail.com>
2025-07-16 22:32:23 +0800

    feat(order): add product_id quantity and order status fields

--- fastapi-demo/services/order-service/app/models.py
+++ fastapi-demo/services/order-service/app/models.py
@@ -1,10 +1,16 @@
 from redis_om import HashModel
 
+ORDER_STATUS_PENDING = "pending"
+ORDER_STATUS_COMPLETED = "completed"
+
 
 class Order(HashModel):
     user_id: int
+    product_id: str
     item: str
     amount: float
+    quantity: int
+    status: str = ORDER_STATUS_PENDING
 
     class Meta:
         global_key_prefix = "order"
